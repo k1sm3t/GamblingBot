7af2295bc3bc3447959733805c8ec22f145bf967
root <root@localhost.localdomain>
2018-10-24 20:41:56 -0300

    Committer:  Arthur Pinheiro First commit. This script scraps the lottery site by looking for the latest X results. Then count how many times each number was drawn.

---
+++ GamblingBot.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+#encoding: utf-8
+import requests
+from optparse import OptionParser
+import time
+import GamblingConstants as constante
+import matplotlib.pyplot as plt
+
+class GamblingBot(object):
+    options = ""
+    args = ""
+    lista_grafico_y = []
+    lista_grafico_x = []
+    def __init__(self, options, args):
+        self.tipo_jogo = options.jogo
+        self.concurso = options.concurso		
+
+    def retorna_url(self, tipo_jogo):
+        if self.tipo_jogo.lower() == "megasena":
+            return constante.url_megasena_parte1 + constante.lista_concursos[0] + constante.url_megasena_parte2
+        elif self.tipo_jogo.lower() == "lotomania":
+            return constante.url_lotomania_parte1 + constante.lista_concursos[1] + constante.url_lotomania_parte2
+        elif self.tipo_jogo.lower() == "lotofacil":
+            return constante.url_lotofacil_parte1 + constante.lista_concursos[2] + constante.url_lotofacil_parte2
+        elif self.tipo_jogo.lower() == "duplasena":
+            return constante.url_duplasena_parte1 + constante.lista_concursos[3] + constante.url_duplasena_parte2
+	
+    def realiza_requests(self):
+        lista_resultados = []
+        lista_resultado_atual = []
+        contador = int(self.concurso)
+        vezes = 0
+        self.string_resultado = lambda x : "resultado" if self.tipo_jogo == "megasena" else ("de_resultado" if self.tipo_jogo == "lotofacil" else None)
+        while contador > int(self.concurso) - 10:
+            vezes += 1
+            self.processa_resposta(lista_resultados, lista_resultado_atual, contador)
+            contador -= 1
+            if (vezes == 5):
+                time.sleep(10)
+                vezes = 0
+                
+        self.conta_ocorrencias(lista_resultados)
+    
+    def processa_resposta(self, lista_resultados, lista_resultado_atual, contador):        
+        resposta = requests.post(self.retorna_url(self.tipo_jogo) + str(contador))
+        resposta = self.parse(resposta)
+        lista_resultado_atual = resposta[self.string_resultado(self.tipo_jogo)].split('-')
+        print(lista_resultado_atual)
+        lista_resultados += lista_resultado_atual
+        
+    def conta_ocorrencias(self, lista_resultados):
+        limite = 0
+        if (self.tipo_jogo == "megasena"):
+            limite = 61
+        elif (self.tipo_jogo == "lotofacil"):
+            limite = 26
+        elif (self.tipo_jogo == "lotomania"):
+            limite = 101
+        elif (self.tipo_jogo == "duplasena"):
+            limite = 51
+        for i in range(1,limite):
+            self.lista_grafico_x.append(i)
+            if (i in range(1,10)):
+                self.lista_grafico_y.append(lista_resultados.count('0' + str(i)))                
+                print(str(i) + ' - ' + str(lista_resultados.count('0' + str(i))))
+            else:
+                self.lista_grafico_y.append(lista_resultados.count(str(i)))
+                print(str(i) + ' - ' + str(lista_resultados.count(str(i))))
+        
+        self.desenha_grafico()
+        
+    def desenha_grafico(self):
+        plt.plot(self.lista_grafico_x, self.lista_grafico_y)
+        plt.title(u"Gráfico de números mais sorteados")
+        plt.grid(True)
+        plt.show()
+    
+    def parse(self, resposta_json):
+        return resposta_json.json()
+
+parser = OptionParser()
+parser.add_option("-j", "--jogo", dest="jogo", help="O tipo do jogo deve ser especificado.")
+parser.add_option("-c", "--concurso", dest="concurso", help="O número do concurso deve ser especificado.")
+(options, args) = parser.parse_args()
+
+gb = GamblingBot(options, args)
+
+gb.realiza_requests()
---
+++ GamblingConstants.py
@@ -0,0 +1,13 @@
+url_lotomania_parte1 = "http://loterias.caixa.gov.br/wps/portal/loterias/landing/"
+url_lotomania_parte2 = "/!ut/p/a1/04_Sj9CPykssy0xPLMnMz0vMAfGjzOLNDH0MPAzcDbz8vTxNDRy9_Y2NQ13CDA38jYEKIoEKnN0dPUzMfQwMDEwsjAw8XZw8XMwtfQ0MPM2I02-AAzgaENIfrh-FqsQ9wBmoxN_FydLAGAgNTKEK8DkRrACPGwpyQyMMMj0VAajYsZo!/dl5/d5/L2dBISEvZ0FBIS9nQSEh/pw/Z7_61L0H0G0JGJVA0AKLR5T3K00V0/res/id=buscaResultado/c=cacheLevelPage/=/?timestampAjax=1538184410074&concurso="
+
+url_lotofacil_parte1 = "http://loterias.caixa.gov.br/wps/portal/loterias/landing/"
+url_lotofacil_parte2 = "/!ut/p/a1/04_Sj9CPykssy0xPLMnMz0vMAfGjzOLNDH0MPAzcDbz8vTxNDRy9_Y2NQ13CDA0sTIEKIoEKnN0dPUzMfQwMDEwsjAw8XZw8XMwtfQ0MPM2I02-AAzgaENIfrh-FqsQ9wBmoxN_FydLAGAgNTKEK8DkRrACPGwpyQyMMMj0VAcySpRM!/dl5/d5/L2dBISEvZ0FBIS9nQSEh/pw/Z7_61L0H0G0J0VSC0AC4GLFAD2003/res/id=buscaResultado/c=cacheLevelPage/=/?timestampAjax=1540420561348&concurso="
+
+url_duplasena_parte1 = "http://loterias.caixa.gov.br/wps/portal/loterias/landing/"
+url_duplasena_parte2 = "/!ut/p/a1/04_Sj9CPykssy0xPLMnMz0vMAfGjzOLNDH0MPAzcDbz8vTxNDRy9_Y2NQ13CDA0sTIEKIoEKnN0dPUzMfQwMDEwsjAw8XZw8XMwtfQ0MPM2I02-AAzgaENIfrh-FqsQ9wBmoxN_FydLAGAgNTKEK8DkRrACPGwpyQyMMMj0VAcySpRM!/dl5/d5/L2dBISEvZ0FBIS9nQSEh/pw/Z7_61L0H0G0J0VSC0AC4GLFAD2003/res/id=buscaResultado/c=cacheLevelPage/=/?timestampAjax=1538217914949&concurso="
+
+url_megasena_parte1 = 'http://loterias.caixa.gov.br/wps/portal/loterias/landing/'
+url_megasena_parte2 = '/!ut/p/a1/04_Sj9CPykssy0xPLMnMz0vMAfGjzOLNDH0MPAzcDbwMPI0sDBxNXAOMwrzCjA0sjIEKIoEKnN0dPUzMfQwMDEwsjAw8XZw8XMwtfQ0MPM2I02-AAzgaENIfrh-FqsQ9wNnUwNHfxcnSwBgIDUyhCvA5EawAjxsKckMjDDI9FQE-F4ca/dl5/d5/L2dBISEvZ0FBIS9nQSEh/pw/Z7_HGK818G0KO6H80AU71KG7J0072/res/id=buscaResultado/c=cacheLevelPage/=/?timestampAjax=1540160525660&concurso='
+
+lista_concursos = ['megasena', 'lotomania', 'lotofacil', 'duplasena']
