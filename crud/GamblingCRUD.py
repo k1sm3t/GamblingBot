7eb2cfb0b52823808c54c781961924ec901c0107
root <root@localhost.localdomain>
2018-10-28 16:11:55 -0300

    Committer: Arthur da Rosa Pinheiro Improvements in the crud class

--- crud/GamblingCRUD.py
+++ crud/GamblingCRUD.py
@@ -8,78 +8,46 @@ Created on Thu Oct 25 18:49:06 2018
 import sqlite3
 
 class GamblingCRUD(object):
+
+    c_create_table = '''
+                            
+        CREATE TABLE {table_name} (
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                numero_concurso TEXT NOT NULL,
+                resultado TEXT NOT NULL
+        );
+                
+                '''
+
+    c_insert_record = 'INSERT INTO {table_name} (numero_concurso, resultado) VALUES ("{a}","{b}")'
+
+    c_retrieve_record = 'SELECT resultado FROM resultados_{game} WHERE numero_concurso = ?'
     
     def connectDB(self):
         self.conn = sqlite3.connect('Gambling.db')    
         self.cursor = self.conn.cursor()
-        if (1 == 0):
-            self.cria_tabelas()
+        if (self.check_table('resultados_megasena') == None):
+            self.cria_tabelas('resultados_megasena')
+            self.cria_tabelas('resultados_lotofacil')
     
-    def cria_tabelas(self):
-        self.cursor.execute('''
-                            
-        CREATE TABLE resultados_megasena (
-                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-                numero_concurso TEXT NOT NULL,
-                resultado TEXT NOT NULL
-        );
-                
-                ''')
+    def cria_tabelas(self, table):
+        self.cursor.execute(self.c_create_table.format(table_name=table))
         
-        self.cursor.execute('''
-                            
-        CREATE TABLE resultados_lotofacil (
-                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-                numero_concurso TEXT NOT NULL,
-                resultado TEXT NOT NULL
-        );
-                
-                ''')
-        
-        self.cursor.execute('''
-                            
-        CREATE TABLE resultados_lotomania (
-                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-                numero_concurso TEXT NOT NULL,
-                resultado TEXT NOT NULL
-        );
-                
-                ''')
-        
-        self.cursor.execute('''
-                            
-        CREATE TABLE resultados_duplasena (
-                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-                numero_concurso TEXT NOT NULL,
-                resultado TEXT NOT NULL
-        );
-                
-                ''')
         
     def insere_registro(self, tipo_jogo, concurso, resultado):
         separador = '-'
         resultado_joinado = separador.join(resultado)
-        if (tipo_jogo.lower() == 'megasena'):
-            self.cursor.execute('INSERT INTO resultados_megasena (numero_concurso, resultado) VALUES ("{a}","{b}")'.format(a=concurso, b=resultado_joinado))
-        elif (tipo_jogo.lower() == 'lotofacil'):
-            self.cursor.execute('INSERT INTO resultados_lotofacil (numero_concurso, resultado) VALUES ("{a}","{b}")'.format(a=concurso, b=resultado_joinado))
-        elif (tipo_jogo.lower() == 'lotomania'):
-            self.cursor.execute('INSERT INTO resultados_lotomania (numero_concurso, resultado) VALUES ("{a}","{b}")'.format(a=concurso, b=resultado_joinado))
-        elif (tipo_jogo.lower() == 'duplasena'):
-            self.cursor.execute('INSERT INTO resultados_duplasena (numero_concurso, resultado) VALUES ("{a}","{b}")'.format(a=concurso, b=resultado_joinado))
-            
+        self.cursor.execute(self.c_insert_record.format(table_name=tipo_jogo.lower(), a=concurso, b=resultado_joinado))            
         self.conn.commit()
+
+    def check_table(self, table):
+        self.cursor.execute('SELECT name FROM sqlite_master WHERE type = "table" AND name = "{table_name}"'.format(table_name=table))
+        return self.cursor.fetchone()
+
             
     def consulta_registro(self, tipo_jogo, concurso):
-        if (tipo_jogo.lower() == 'megasena'):
-            sql = 'SELECT resultado FROM resultados_megasena WHERE numero_concurso = ?'
-        elif (tipo_jogo.lower() == 'lotofacil'):
-            sql = 'SELECT resultado FROM resultados_lotofacil WHERE numero_concurso = ?'
-        elif (tipo_jogo.lower() == 'lotomania'):
-            sql = 'SELECT resultado FROM resultados_lotomania WHERE numero_concurso = ?'
-            
-        self.cursor.execute(sql, [(concurso)])
-        
+        sql = self.c_retrieve_record.format(game=tipo_jogo.lower())            
+        self.cursor.execute(sql, [(concurso)])        
         return self.cursor.fetchone()
     
     def close_connection(self):
